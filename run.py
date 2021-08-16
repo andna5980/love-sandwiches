3be533d795cbb79987d9e0d525fe63ff78b9b80d
andna5980 <andy5980@hotmail.com>
2021-08-16 15:50:06 +0000

    Add sales data to sales worsheet

--- run.py
+++ run.py
@@ -12,8 +12,6 @@ SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-
-
 def get_sales_data():
     """
     Get sales figures input fro the user
@@ -55,5 +53,16 @@ def validate_data(values):
     return True        
 
 
-data = get_sales_data()
+def update_sales_worksheet(data):
+    """
+    Update sales worsheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated succesfully.\n")
 
+
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
